136aaa917cc078208b371145e00995fe5a634213
Zlata <shklyayeva.zlata@mail.ru>
2026-05-26 20:04:35 +0300

    Рефакторингг

--- js/thumbnails.js
+++ js/thumbnails.js
@@ -1,13 +1,24 @@
 import {generatePhotos} from './data.js';
-const container = document.querySelector('.pictures');
+const picturesContainer = document.querySelector('.pictures');
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const randomPictureFragment = document.createDocumentFragment();
 
-const randomPictures = generatePhotos();
-randomPictures.forEach((randomPhoto) => {
-  const randomPicture = pictureTemplate.cloneNode(true);
-  randomPicture.querySelector('.picture__img').src = randomPhoto.url;
-  randomPicture.querySelector('.picture__img').alt = randomPhoto.description;
-  randomPicture.querySelector('.picture__likes').textContent = randomPhoto.likes;
-  randomPicture.querySelector('.picture__comments').textContent = randomPhoto.comments.length;
-  container.appendChild(randomPicture);
-});
+
+const renderPictures = () => {
+  const randomPictures = generatePhotos();
+  randomPictures.forEach((randomPhoto) => {
+    const randomPicture = pictureTemplate.cloneNode(true);
+    randomPicture.querySelector('.picture__img').src = randomPhoto.url;
+    randomPicture.querySelector('.picture__img').alt = randomPhoto.description;
+    randomPicture.querySelector('.picture__likes').textContent = randomPhoto.likes;
+    randomPicture.querySelector('.picture__comments').textContent = randomPhoto.comments.length;
+    randomPictureFragment.appendChild(randomPicture);
+  });
+
+  picturesContainer.appendChild(randomPictureFragment);
+};
+
+renderPictures();
+
+export { renderPictures };
+
